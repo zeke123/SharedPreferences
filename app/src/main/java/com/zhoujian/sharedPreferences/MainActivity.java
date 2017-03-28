73f13de48101bae8afc13b426fbfaa6afcd48f6e
zeke123 <zekezhoujian@163.com>
2017-03-28 15:21:40 +0800

    提交代码

--- app/src/main/java/com/zhoujian/sharedPreferences/MainActivity.java
+++ app/src/main/java/com/zhoujian/sharedPreferences/MainActivity.java
@@ -8,11 +8,12 @@ import android.widget.Button;
 public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button forceOffline = (Button) findViewById(R.id.force_offline);
-        forceOffline.setOnClickListener(new View.OnClickListener() {
+        Button mButton = (Button) findViewById(R.id.force_offline);
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent("com.zhoujian.sharedPreferences.FORCE_OFFLINE");
