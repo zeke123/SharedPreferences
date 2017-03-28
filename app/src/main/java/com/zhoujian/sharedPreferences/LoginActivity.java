91ed896fb9b18f4ebb704c1b32c276aa0498c94f
zeke123 <zekezhoujian@163.com>
2017-03-28 15:20:38 +0800

    提交代码

--- app/src/main/java/com/zhoujian/sharedPreferences/LoginActivity.java
+++ app/src/main/java/com/zhoujian/sharedPreferences/LoginActivity.java
@@ -16,41 +16,41 @@ public class LoginActivity extends BaseActivity {
 
     private SharedPreferences.Editor editor;
 
-    private EditText accountEdit;
+    private EditText mAccount;
 
-    private EditText passwordEdit;
+    private EditText mPassword;
 
     private Button login;
 
-    private CheckBox rememberPass;
+    private CheckBox mCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         pref = PreferenceManager.getDefaultSharedPreferences(this);
-        accountEdit = (EditText) findViewById(R.id.account);
-        passwordEdit = (EditText) findViewById(R.id.password);
-        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
+        mAccount = (EditText) findViewById(R.id.account);
+        mPassword = (EditText) findViewById(R.id.password);
+        mCheckBox = (CheckBox) findViewById(R.id.remember_pass);
         login = (Button) findViewById(R.id.login);
         boolean isRemember = pref.getBoolean("remember_password", false);
         if (isRemember) {
             // 将账号和密码都设置到文本框中
             String account = pref.getString("account", "");
             String password = pref.getString("password", "");
-            accountEdit.setText(account);
-            passwordEdit.setText(password);
-            rememberPass.setChecked(true);
+            mAccount.setText(account);
+            mPassword.setText(password);
+            mCheckBox.setChecked(true);
         }
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String account = accountEdit.getText().toString();
-                String password = passwordEdit.getText().toString();
+                String account = mAccount.getText().toString();
+                String password = mPassword.getText().toString();
                 // 如果账号是123456且密码是123456，就认为登录成功
                 if (account.equals("123456") && password.equals("123456")) {
                     editor = pref.edit();
-                    if (rememberPass.isChecked()) { // 检查复选框是否被选中
+                    if (mCheckBox.isChecked()) { // 检查复选框是否被选中
                         editor.putBoolean("remember_password", true);
                         editor.putString("account", account);
                         editor.putString("password", password);
